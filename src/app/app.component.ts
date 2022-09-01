9e7a46c1277752aec5650a4fb7910a72976139da
Kyllian <kyllian.gt@hotmail.fr>
2022-09-02 01:37:44 +0200

    Setup proxy.conf.json and hello world with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Observable, pluck } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'jarvis-web-client';
+
+  backendAnswer$: Observable<string>;
+
+  constructor(
+    private readonly _http: HttpClient
+  ) {
+    this.backendAnswer$ = this._http.get<{ answer: string }>(environment.backendUrl).pipe(pluck('answer'));
+  }
 }
